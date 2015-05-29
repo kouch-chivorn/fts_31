5ca01b3f2672a58327e8bb0f51a986c20f5b7f18
kouch-chivorn <kouch.chivorn@framgia.com>
2015-05-29 11:21:32 +0700

    create sign-up page

--- app/models/answer.rb
+++ app/models/answer.rb
@@ -1,6 +1,3 @@
 class Answer < ActiveRecord::Base
   belongs_to :question
-  has_many :test_questions
-  
-  validates_presence_of :content
 end
--- db/migrate/20150528095519_create_tests.rb
+++ db/migrate/20150528095519_create_tests.rb
@@ -1,11 +1,13 @@
 class CreateTests < ActiveRecord::Migration
   def change
     create_table :tests do |t|
-      t.references :user, index: true, foreign_key: true
-      t.references :category, index: true, foreign_key: true
+      t.references :user, index: true
+      t.references :category, index: true
       t.integer :result
 
       t.timestamps null: false
     end
+    add_foreign_key :tests, :users
+    add_foreign_key :tests, :categories
   end
 end

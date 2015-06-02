cc4e1456ad892e2b54c7545e7b1b835754c44470
kouch-chivorn <kouch.chivorn@framgia.com>
2015-06-02 16:07:09 +0700

    admin sign-in sign-out

--- db/seeds.rb
+++ db/seeds.rb
@@ -3,5 +3,8 @@
 #
 # Examples:
 #
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+   # cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
+   # Mayor.create(name: 'Emanuel', city: cities.first)
+
+Admin.create(name: "kouch chivorn", email: "kouchchivorn@yahoo.com",
+            password: "12345678", password_confirmation: "12345678")
